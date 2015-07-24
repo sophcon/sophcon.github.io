392ef8b48db2534ed07cc66f726030fc6233e3fd
Yer Yang <yangyer81@gmail.com>
2015-07-24 10:56:26 -0700

    Added blog and resolved menu

--- js/sophcon.js
+++ js/sophcon.js
@@ -1,5 +1,35 @@
+jQuery("#layerslider").layerSlider({
+  pauseOnHover: true,
+  autoPlayVideos: false,
+  skinsPath: '{{base}}/assets/layerslider/skins/',
+  responsive: false,
+  responsiveUnder: 1280,
+  layersContainer: 1280,
+  skin: 'borderlessdark3d',
+  hoverPrevNext: true,
+});
+
 $(document).ready(function() {
-  $('.navigate-on-click').click(function() {
-    window.location.href = $(this).attr('href');
+  function bindNavbar() {
+    if ($(window).width() > 768) {
+      $('.navbar-default .dropdown').on('mouseover', function() {
+        $('.dropdown-toggle', this).next('.dropdown-menu').show();
+      }).on('mouseout', function() {
+        $('.dropdown-toggle', this).next('.dropdown-menu').hide();
+      });
+      $('.dropdown-toggle').click(function() {
+        if ($(this).next('.dropdown-menu').is(':visible')) {
+          window.location = $(this).attr('href');
+        }
+      });
+    } else {
+      $('.navbar-default .dropdown').off('mouseover').off('mouseout').off('click');
+    }
+  }
+
+  $(window).resize(function() {
+    bindNavbar();
   });
+
+  bindNavbar();
 });
